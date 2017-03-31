cc4cb6e322a89149c448a075e2b86b40e86846f7
Subash Shahi <subashsly33@gmail.com>
2017-03-31 09:44:16 +0545

    final commit

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -27,6 +27,7 @@ config.assets.digest = true
 
   # Do not fallback to assets pipeline if a precompiled asset is missed.
   config.assets.compile = true
+  config.assets.compress = true
 
   # `config.assets.precompile` and `config.assets.version` have moved to config/initializers/assets.rb
 
