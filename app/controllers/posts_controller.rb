790a167595d7d44398bf6ef78444a2162c95d91d
Subash Shahi <subashsly33@gmail.com>
2017-03-31 10:30:52 +0545

    final

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,11 +1,11 @@
 class PostsController < ApplicationController
  
 	def create
-		
+		UserMailer.welcome_email(params[:post].inspect).deliver_later
      	respond_to do |format|
-     		if UserMailer.welcome_email(params[:post].inspect).deliver_later
+
      			format.js
-     		end
+     		
         	
        	end
  	end
