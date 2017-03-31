c226fcc64853b82edc1d649a1177e2d4f5df6d0c
Subash Shahi <subashsly33@gmail.com>
2017-03-31 10:42:37 +0545

    final

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,12 +1,10 @@
 class PostsController < ApplicationController
  
 	def create
-		UserMailer.welcome_email(params[:post].inspect).deliver_later
+		UserMailer.welcome_email(params[:email]).deliver_later
+		
      	respond_to do |format|
-
      			format.js
-     		
-        	
        	end
  	end
 end
