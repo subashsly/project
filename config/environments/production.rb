adee2d454316864ecd0a913aec55a57d31e10b4f
Subash Shahi <subashsly33@gmail.com>
2017-04-06 15:57:14 +0545

    final

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -51,15 +51,16 @@ config.assets.digest = true
 
 
 config.action_mailer.default_url_options = { :host => 'projectnb.herokuapp.com' }  
-config.action_mailer.delivery_method = :smtp  
-config.action_mailer.perform_deliveries = true  
+# ActionMailer Config
+# Setup for production - deliveries, no errors raised
+config.action_mailer.delivery_method = :smtp
+config.action_mailer.perform_deliveries = true
 config.action_mailer.raise_delivery_errors = false
-config.active_record.dump_schema_after_migration = false
-config.action_mailer.default :charset => "utf-8"  
-config.action_mailer.smtp_settings = {  
+config.action_mailer.default :charset => "utf-8" 
+config.action_mailer.smtp_settings = {
   address: "smtp.gmail.com",
   port: 587,
-  domain: "gmail.com",
+  domain: "projectnb.herokuapp.com",
   authentication: "plain",
   enable_starttls_auto: true,
   user_name: ENV["GMAIL_USERNAME"],
